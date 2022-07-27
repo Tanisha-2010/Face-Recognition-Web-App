059fae7dc87c05d0f8997c4f992bdb2cfc02d86a
Tanisha-2010 <104196498+Tanisha-2010@users.noreply.github.com>
2022-07-27 21:45:14 +0530

    Add files via upload
    main.js is updated

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 ///https: //teachablemachine.withgoogle.com/models/zRXrMOkD8/
+
+console.log(ml5.version);
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: "png",
+    png_quality: 90
+});
+
+camera = document.getElementById("webcam");
+
+Webcam.attach("#camera");
+
+function take_snapShot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = "<img id='picture' src='" + data_uri + "'>"
+    });
+}
+
+classifier = ml5.imageClassifier("https: //teachablemachine.withgoogle.com/models/zRXrMOkD8/", modelLoaded);
+
+function modelLoaded() {
+    console.log("Model is Loaded");
+}
